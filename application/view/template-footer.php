cb96a7d7a6ba5f9a960132f1e033cf3360a809b6
Adam Patterson <hello@adampatterson.ca>
2011-09-07 13:43:30 -0600

    Updated the Donation URL to use WePay

--- application/view/template-footer.php
+++ application/view/template-footer.php
@@ -11,12 +11,7 @@
         <a href="#" class="help"><img src="<?=BASE_URL; ?>assets/images/icons/25-bug.png" width="25" height="25" alt="Submit a Bug"/></a>
     </div>
     <div class="span-1 last">
-        <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-        <input type="hidden" name="cmd" value="_s-xclick">
-        <input type="hidden" name="hosted_button_id" value="8AUKFFQFLWHKQ">
-        <input type="image" src="http://addressbook.adampatterson.ca/25-5dolar.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-        <img alt="" border="0" src="https://www.paypalobjects.com/WEBSCR-640-20110401-1/en_US/i/scr/pixel.gif" width="1" height="1">
-        </form>
+        <a href="https://www.wepay.com/donate/112338" target="_blank"><img src="http://addressbook.adampatterson.ca/25-5dolar.png" alt="Donate" /></a>
     </div>
 <!--  Render time
             <?
